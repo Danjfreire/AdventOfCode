e63c9b3fbe3f2c335a441ca50c57a4c1df22e4b7
Daniel Freire <danjfreire@gmail.com>
2023-12-30 10:22:44 -0300

    refactor: refactored 2015.11 js answer

--- 2015/day11/js/2015.11.mjs
+++ 2015/day11/js/2015.11.mjs
@@ -1,10 +1,58 @@
+const FORBIDDEN = ["i", "o", "l"];
+
 /**
  * @param {string} currentPassword
+ * @returns {boolean}
+ */
+function isValid(passwordArray) {
+  const hasForbidden = FORBIDDEN.some((char) => passwordArray.includes(char));
+
+  if (hasForbidden) {
+    return false;
+  }
+
+  let hasIncreasingStraight = false;
+  let consideredPairs = [];
+  let pairCount = 0;
+
+  let currentCode;
+  let previousCode;
+  let prePreviousCode;
+  for (let i = 1; i < passwordArray.length; i++) {
+    currentCode = passwordArray[i].charCodeAt(0);
+    previousCode = passwordArray[i - 1].charCodeAt(0);
+
+    // check if it is a two pair sequence
+    if (
+      currentCode == previousCode &&
+      !consideredPairs.includes(passwordArray[i])
+    ) {
+      pairCount++;
+      consideredPairs.push(passwordArray[i]);
+    }
+
+    // check if it is an increasing straight
+    if (i >= 2) {
+      prePreviousCode = passwordArray[i - 2].charCodeAt("0");
+      if (
+        currentCode - previousCode === 1 &&
+        previousCode - prePreviousCode === 1
+      ) {
+        hasIncreasingStraight = true;
+      }
+    }
+  }
+
+  return hasIncreasingStraight && pairCount >= 2;
+}
+
+/**
+ * @param {string} currentPassword
+ * @returns {string} next valid password
  */
 function getNextValidPassword(currentPassword) {
   const min = "a".charCodeAt(0);
   const max = "z".charCodeAt(0);
-  const forbidden = ["i".charCodeAt(0), "o".charCodeAt(0), "l".charCodeAt(0)];
 
   const passwordArray = currentPassword.split("");
   let isPasswordValid = false;
@@ -24,49 +72,12 @@ function getNextValidPassword(currentPassword) {
     }
 
     // 2 - Check if password is valid
-    const hasForbidden = forbidden.some((charCode) =>
-      passwordArray.includes(String.fromCharCode(charCode))
-    );
-    let hasIncreasingStraight = false;
-    let consideredPairs = [];
-    let pairCount = 0;
-
-    let currentCode;
-    let previousCode;
-    let prePreviousCode;
-    for (let i = 0; i < passwordArray.length; i++) {
-      currentCode = passwordArray[i].charCodeAt(0);
-
-      // check if it is a two pair sequence
-      if (i > 0) {
-        previousCode = passwordArray[i - 1].charCodeAt(0);
-        if (
-          currentCode == previousCode &&
-          !consideredPairs.includes(passwordArray[i])
-        ) {
-          pairCount++;
-          consideredPairs.push(passwordArray[i]);
-        }
-      }
-
-      // check if it is an increasing straight
-      if (i >= 2) {
-        prePreviousCode = passwordArray[i - 2].charCodeAt("0");
-        if (
-          currentCode - previousCode === 1 &&
-          previousCode - prePreviousCode === 1
-        ) {
-          hasIncreasingStraight = true;
-        }
-      }
-    }
-
-    isPasswordValid = !hasForbidden && hasIncreasingStraight && pairCount >= 2;
+    isPasswordValid = isValid(passwordArray);
   }
 
   return passwordArray.join("");
 }
 
-const password = "hepxxyzz";
+const password = "hepxcrrq";
 const nextPassword = getNextValidPassword(password);
 console.log("Next valid Password : ", nextPassword);
